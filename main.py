dde23e6d2ba5a4af3bbaf7c12b1068f038deae38
mr.tilabaldyev <mr.tilabaldyev@gmail.com>
2024-01-11 12:40:08 +0600

    I put in my calculator

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def sum(value1, value2):
-    return f"Result sum: {value1 + value2}"
+def sum(value1, value2, operation):
+    if operation == "*":
+        return f"Result: { value1 * value2 }"
+
+    if operation == "/":
+        return f"Result: { int(value1 / value2) }"
+
+    if operation == "-":
+        return f"Result: { value1 - value2 }"
+
+    if operation == "+":
+        return f"Result: { value1 + value2 }"
+    else:
+        return f"Error!"
 
 
-x1 = int(input("Enter your values 1: "))
-x2 = int(input("Enter your values 2: "))
-print(sum(x1, x2))
+x1 = int(input("Enter first value: "))
+x2 = int(input("Enter second value: "))
+operation = input("Select operation (* / - +): ")
+
+print(sum(x1, x2, operation))
