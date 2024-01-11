d5b30f003b44234312885d01083d2ce38d71d6ec
mr.Islambek <80396084+mr-islambek@users.noreply.github.com>
2024-01-11 12:11:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def sum(value1, value2):
     return f"{value1 + value2}"
-
-
-x1 = int(input("Enter values 1: "))
-x2 = int(input("Enter values 2: "))
+    
+x1 = int(input("Enter your values 1: "))
+x2 = int(input("Enter your values 2: "))
 print(sum(x1, x2))
